b2601814abe0ffb79d8bf154d332875f93c1de1d
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-08-24 15:38:01 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gopal-1.onrender.com"], 
+    allow_origins=["https://gopalproject-1.onrender.com"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
